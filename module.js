dbba7b42211f099454cdf909db8770af1be04fed
nnmrts <nanomiratus@gmail.com>
2021-11-26 02:09:55 +0100

    feat: ♻️ rewrite everything
    error handling, options object argument, better readme

--- module.js
+++ module.js
@@ -1,15 +1,84 @@
 const {
+	env,
+	errors: {
+		AddrInUse
+	},
 	listenDatagram
 } = Deno;
 
-export default async (style, anybarPort = 1738, denoPort = 8371) => {
-	const connection = listenDatagram({ transport: "udp", port: denoPort });
+const setupConnection = (port) => listenDatagram({ transport: "udp", port });
+
+const defaultAnybarPort = Number(env.get("ANYBAR_PORT")) || 1738;
+
+const minimumRegisteredPort = 2 ** 10;
+const minimumDynamicPort = (2 ** 15) + (2 ** 14);
+const maximumPort = (2 ** 16) - 1;
+
+/** 
+ * @param {(
+ * 	"white"|
+ * 	"red"|
+ * 	"orange"|
+ * 	"yellow"|
+ * 	"green"|
+ * 	"cyan"|
+ * 	"blue"|
+ * 	"purple"|
+ * 	"black"|
+ * 	"question"|
+ * 	"exclamation"|
+ * 	"filled"|
+ * 	"hollow"
+ * )} style - The style of the dot you want AnyBar to display.
+ * @param {object} [ports={anybar:Number(Deno.env.get("ANYBAR_PORT"))||1738}] - The port options.
+ * @param {number} [ports.anybar=Number(Deno.env.get("ANYBAR_PORT"))||1738]
+ * @param {number} [deno]
+ * 
+ * @returns {Promise<void>}
+ */
+export default async (style, {
+	anybar = defaultAnybarPort,
+	deno
+} = {
+		anybar: defaultAnybarPort
+	}) => {
+	let currentDenoPort = deno;
+	let connection;
+
+	if (typeof currentDenoPort === "undefined") {
+		for (currentDenoPort = minimumDynamicPort; currentDenoPort <= maximumPort; currentDenoPort++) {
+			try {
+				connection = setupConnection(currentDenoPort);
+
+				break;
+			} catch (error) {
+				if (!(error instanceof AddrInUse)) {
+					throw error;
+				}
+			}
+		}
+
+		if (currentDenoPort > maximumPort) {
+			throw new RangeError(`No free ports in range ${minimumDynamicPort}-${maximumPort}. How? 🤯`);
+		}
+	}
+	else {
+		if (deno < minimumRegisteredPort) {
+			throw new RangeError(`Port numbers below ${minimumRegisteredPort}, like ${deno}, are not supported.`);
+		}
+
+		if (deno > maximumPort) {
+			throw new RangeError(`Port numbers above ${maximumPort}, like ${deno}, are invalid.`);
+		}
+
+		connection = setupConnection(currentDenoPort);
+	}
 
 	const encoder = new TextEncoder();
 
 	const message = encoder.encode(style);
 
-	await connection.send(message, { transport: "udp", port: anybarPort });
+	await connection.send(message, { transport: "udp", port: anybar });
 
 	connection.close();
 }
--- readme.md
+++ readme.md
@@ -5,40 +5,54 @@ Control [AnyBar](https://github.com/tonsky/AnyBar)
 ## Usage
 
 ```js
-import anybar from "https://x.nest.land/anybar@0.1.3/module.js";
+import anybar from "https://x.nest.land/anybar@1.0.0/module.js";
 
-anybar("purple");
-// The Anybar menu bar icon turned purple
+anybar("red");
+// This turns AnyBar menu bar dot red.
+// 🔴
+
+anybar("green", {anybar: 9876});
+// AnyBar could run on a specific port.
+// 🟢
+
+anybar("blue", {anybar: 9876, deno: 54321});
+// If this module needs to run on a specific port for some reason.
+// 🔵
+
 ```
 
 ## API
 
-### anybar(style, anybarPort, denoPort)
+### anybar(style, ports)
 
-Returns a promise, but AnyBar doesn't send back a reply, so really the only point of waiting for the promise to resolve is in case of an obscure DNS error.
+- Returns: A promise that resolves after the style got sent to AnyBar.
 
 #### style
 
-Type: `string`
+- Type: `string`
 
 The [style](https://github.com/tonsky/AnyBar#usage) of the dot you want AnyBar to display.
 
-#### anybarPort
+#### ports
 
-Type: `number`
+- Type: `object`
 
-Default: `1738`
+The port options.
+
+##### ports.anybar
+
+- Type: `number`
+- Default: The value of the environment variable `ANYBAR_PORT` or, if that is undefined, `1738`, which is the default port number for AnyBar.
 
 The UDP port AnyBar runs on.
 
-#### denoPort
+##### ports.deno
 
-Type: `number`
+- Type: `number`
+- Default: The lowest free port number in the ["unassignable"](https://www.rfc-editor.org/rfc/rfc6335.html#section-8.1.2) range (`49152`-`65535`).
 
-Default: `8371`
-
-The port this module uses to communicate with AnyBar.
+The port this module should use to communicate with AnyBar. Only numbers between 1024 and 65535 are allowed, anything else will throw a `RangeError`.
 
 ## Acknowledgments
 
-This module, especially the API, is heavily inspired by the [node module for the same purpose](https://github.com/sindresorhus/anybar) by [sindresorhus](https://github.com/sindresorhus).
+This module, especially the API, was heavily inspired by the [node module for the same purpose](https://github.com/sindresorhus/anybar) by [sindresorhus](https://github.com/sindresorhus).
